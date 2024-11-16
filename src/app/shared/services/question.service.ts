4565c581828100a66b390a0014ac15b6d549092f
Adriel Vinicius <adriel1234.1234@gmail.com>
2024-11-16 15:19:40 -0400

    add question service

---
+++ src/app/shared/services/question.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionService {
+
+  private apiUrl = 'http://localhost:8000/api/question/';
+
+  constructor(private http: HttpClient) { }
+
+  createQuestion(questionData: any): Observable<any> {
+    return this.http.post(this.apiUrl, questionData);
+  }
+  getQuestions(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  updateQuestion(id: number, question: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}${id}/`, question);
+  }
+
+  deleteQuestion(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}${id}/`);
+  }
+
+}
