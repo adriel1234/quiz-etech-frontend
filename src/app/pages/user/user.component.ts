34a61561afd27313a8df080de3b0191e3d791162
adriel <adriel1234.1234@gmail.com>
2024-11-21 20:39:05 -0400

    add logout

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import {QuestionAnswerFormComponent} from '../../components/question-list/question-answer-form/question-answer-form.component';
 import {NavigationExtras, Router, RouterOutlet} from '@angular/router';
-import {NgClass} from '@angular/common';
+import {NgClass, NgIf} from '@angular/common';
 
 
-import {MatButton} from '@angular/material/button';
+import {MatButton, MatIconButton} from '@angular/material/button';
 import {GroupQuestionListComponent} from '../../components/question-group-list/group-question-list.component';
 import {
   QuestionGroupItemComponent
 } from '../../components/question-group-list/question-group-item/question-group-item.component';
+import {MatIcon} from '@angular/material/icon';
 
 interface Menu {
   title: string;
@@ -25,7 +26,10 @@ interface Menu {
     QuestionAnswerFormComponent,
     GroupQuestionListComponent,
     QuestionGroupItemComponent,
-    MatButton
+    MatButton,
+    MatIconButton,
+    MatIcon,
+    NgIf
   ],
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
@@ -47,5 +51,17 @@ export class UserComponent {
 
   public goToPage(route: string): void {
     this.router.navigate([route]).then();
+  }
+
+  public isLoggedIn(): boolean {
+    // Verificar se o token de autenticação está presente no sessionStorage
+    return !!sessionStorage.getItem('auth-token');
+  }
+  public deslogar(): void {
+    // Limpar o sessionStorage para remover os dados do usuário (tokens)
+    sessionStorage.clear();
+
+    // Redirecionar para a página de login
+    this.router.navigate(['/login']);
   }
 }
