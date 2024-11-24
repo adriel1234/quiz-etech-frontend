5e8c86fb491aa422b88371e6042daaeb050856ee
Adriel Vinicius <adriel1234.1234@gmail.com>
2024-11-24 15:52:24 -0400

    update group question

--- src/app/components/question-group-list/question-group-item/question-group-item.component.ts
+++ src/app/components/question-group-list/question-group-item/question-group-item.component.ts
@@ -58,9 +58,12 @@ import {MatOption, MatSelect} from '@angular/material/select';
 export class QuestionGroupItemComponent extends BaseComponent<QuestionGroup> implements OnInit {
   public formGroup: FormGroup;
   public questions: Question[] = [];
+  public questionGroupId?: number;
 
 
-  constructor(http: HttpClient,    private questionService: QuestionService,  // Injeção do QuestionService
+  constructor(http: HttpClient,
+              private questionService: QuestionService,  // Injeção do QuestionService
+              private route: ActivatedRoute,
               private snackBar: MatSnackBar) {
     super(http, URLS.GROUP); // Certifique-se de que a URL está correta
     this.formGroup = new FormGroup({
@@ -70,20 +73,44 @@ export class QuestionGroupItemComponent extends BaseComponent<QuestionGroup> imp
   }
 
   ngOnInit(): void {
+    this.questionGroupId = Number(this.route.snapshot.paramMap.get('id'));
     if (this.object.id) {
       this.formGroup.patchValue({
         description: this.object.description,
         questions_group_question: this.object.questions_group_question || [],
       });
     }
-    this.loadQuestions();  // Carregar perguntas ao inicializar
+
+    if (this.questionGroupId) {
+      this.loadQuestionsData(); // Carregar dados do QuestionGroup
+    } else {
+      this.loadQuestions(); // Carregar perguntas para um novo QuestionGroup
+    }
   }
+
+  loadQuestionsData(): void {
+    this.service.getById(this.questionGroupId!).subscribe(
+      (data: QuestionGroup) => {
+        this.object = data;  // Atribuindo o QuestionGroup carregado à variável 'object'
+        this.formGroup.patchValue({
+          description: data.description,
+          questions_group_question: data.questions_group_question || [],
+        });
+        this.loadQuestions(); // Carregar todas as perguntas, algumas podem já estar selecionadas
+      },
+      (error) => {
+        console.error('Erro ao carregar QuestionGroup', error);
+        this.snackBar.open('Erro ao carregar o grupo de perguntas', 'Fechar', { duration: 3000 });
+      }
+    );
+  }
+
 
   loadQuestions(): void {
     this.questionService.getQuestions().subscribe(
       (data: Question[]) => {
         this.questions = data;
-        console.log(data)// Atribuindo as perguntas à variável
+        console.log(data);  // Atribuindo as perguntas à variável
       },
       (error) => {
         console.error('Erro ao carregar perguntas', error);
@@ -93,7 +120,6 @@ export class QuestionGroupItemComponent extends BaseComponent<QuestionGroup> imp
       }
     );
   }
-
 
   public save(): void {
     if (this.formGroup.valid) {
@@ -106,15 +132,33 @@ export class QuestionGroupItemComponent extends BaseComponent<QuestionGroup> imp
 
   saveOrUpdate(): void {
     if (this.formGroup.valid) {
-      this.validateForm();
+      this.validateForm();  // Valida o formulário antes de enviar
+
+      // Garantir que o campo `questions_group_question` tenha os IDs das questões selecionadas
+      this.object.questions_group_question = this.formGroup.get('questions_group_question')?.value;
+
       if (this.object.id) {
-        this.service.update(this.object.id!, this.object).subscribe((response) => {
-          this.goToPage('question-groups');
-        });
+        this.service.update(this.object.id!, this.object).subscribe(
+          (response) => {
+            this.snackBar.open('Grupo de perguntas atualizado com sucesso', 'Fechar', { duration: 3000 });
+            this.goToPage('question-groups');
+          },
+          (error) => {
+            console.error('Erro ao atualizar o grupo de perguntas', error);
+            this.snackBar.open('Erro ao atualizar o grupo de perguntas', 'Fechar', { duration: 3000 });
+          }
+        );
       } else {
-        this.service.save(this.object).subscribe((response) => {
-          this.goToPage('question-groups');
-        });
+        this.service.save(this.object).subscribe(
+          (response) => {
+            this.snackBar.open('Grupo de perguntas salvo com sucesso', 'Fechar', { duration: 3000 });
+            this.goToPage('question-groups');
+          },
+          (error) => {
+            console.error('Erro ao salvar o grupo de perguntas', error);
+            this.snackBar.open('Erro ao salvar o grupo de perguntas', 'Fechar', { duration: 3000 });
+          }
+        );
       }
     }
   }
