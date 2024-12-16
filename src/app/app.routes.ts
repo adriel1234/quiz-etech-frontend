6f39f4a9f21d151e13406bab386350e92497211a
Adriel Vinicius <adriel1234.1234@gmail.com>
2024-12-15 22:14:02 -0400

    ajuste ranking list match

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import {Routes} from '@angular/router';
 import {LoginComponent} from './pages/login/login.component';
 import {SignUpComponent} from './pages/signup/signup.component';
-// import {UserComponent} from './pages/user/user.component';
 import {AuthGuard} from './shared/services/auth-guard.service';
 import {
   QuestionAnswerFormComponent
@@ -12,6 +11,12 @@ import {
 } from './components/question-group-list/question-group-item/question-group-item.component';
 import {GroupQuestionListComponent} from './components/question-group-list/group-question-list.component';
 import {AdminComponent} from './pages/admin/admin.component';
+import {MatchComponent} from './components/match/match.component';
+import {MatchItemComponent} from './components/match/match-item/match-item.component';
+import {JoinComponent} from './pages/player/components/join/join.component';
+import {InfoComponent} from './pages/player/components/info/info.component';
+import {ScoreComponent} from './pages/player/components/score/score.component';
+import {QuizComponent} from './pages/player/components/quiz/quiz.component';
 import {RankingComponent} from './components/ranking/ranking.component';
 
 export const routes: Routes = [
@@ -71,11 +76,57 @@ export const routes: Routes = [
         data: { title: 'Editar Quizz' },
       },
       {
+        path: 'match',
+        component: MatchComponent,
+        data: { title: 'Match Quizz' },
+      }
+      ,
+      {
+        path: 'match/:action',
+        component: MatchItemComponent,
+        data: { title: 'Match Criar' },
+      },
+      {
+        path: 'match/:action/:id',
+        component: MatchItemComponent,
+        data: { title: 'Editar Match' },
+      },
+      {
         path: 'ranking/:id',
         component: RankingComponent,
         data: { title: 'Ranking' },
       }
 
     ]
-  }
+  },
+  {
+    path: 'player',
+    children: [
+      {
+        path: '',
+        redirectTo: 'join',
+        pathMatch: 'full',
+      },
+      {
+        path: 'join',
+        component: JoinComponent,
+        data: { title: 'Autenticação' },
+      },
+      {
+        path: 'info',
+        component: InfoComponent,
+        data: { title: 'Sala de Espera' },
+      },
+      {
+        path: 'quiz',
+        component: QuizComponent,
+        data: { title: 'Respostas' },
+      },
+      {
+        path: 'score',
+        component: ScoreComponent,
+        data: { title: 'Total de respostas certas' },
+      },
+    ],
+  },
 ];
